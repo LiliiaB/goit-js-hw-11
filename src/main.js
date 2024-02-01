86c2cfbc5f3f323e3308061618e8e686cc5ad308
Lilia Levchenko <liliya.benatti@gmail.com>
2024-02-01 21:49:50 +0100

    css added

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const fetchUsersBtn = document.querySelector('.button');
+
+/* const options = {
+	method: "GET"
+};
+
+fetch("<https://jsonplaceholder.typicode.com/users>", options)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
+
+  */
